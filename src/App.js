dfe67c5e93a8e8624302567fd66c64397e0f5862
Gabriel Johnson <leirbag75@gmail.com>
2020-05-27 18:54:33 -0700

    Replace dummy implementation of App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import componentize from './componentize';
+import Http from './http';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+
+  componentDidMount() {
+    global.http = new Http(this).start();
+  }
+
   render() {
-    return <span style={{color: "red"}}>Hi</span>;
+    return <React.Fragment>
+      {this.state.data.map(item => componentize(item))}
+      </React.Fragment>;
   }
 
 }
